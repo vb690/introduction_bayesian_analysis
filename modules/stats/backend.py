149ad1e217ef89c6f1fa8744d2ef5d26cd61fcfe
vb690 <vb690@york.ac.uk>
2020-08-28 13:34:32 +0100

    Added stats utils and binomial update visualizer

--- modules/stats/backend.py
+++ modules/stats/backend.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 
+def compute_stats(array, ci=95, precis=2):
+    """
+    """
+    array = np.array(array)
+    stats = {
+        'mean': array.mean(),
+        'std': array.std(),
+        'median': np.median(array),
+        'lower': np.percentile(array, 100 - ci),
+        'upper': np.percentile(array, ci)
+    }
+    for stat, value in stats.items():
+
+        stats[stat] = np.round(value, precis)
+
+    return stats
+
+
 def compute_sp_posterior(prior, likelyhood):
     """ Compute posterior probability
 
--- modules/visuals.py
+++ modules/visuals.py
@@ -1,9 +1,13 @@
 import numpy as np
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+from scipy.stats import binom
 
-from modules.stats.backend import compute_sp_posterior
+import matplotlib.pyplot as plt
+
+from IPython.display import display, clear_output
+
+from modules.stats.backend import compute_stats
+
 
 def visualize_grid_search():
     """
@@ -11,55 +15,73 @@ def visualize_grid_search():
     pass
 
 
-def visualize_sp_update(prior, likelyhood, parameter_space,
-                        figsize=(10, 10), **kwargs):
+def visualize_binomial_update(n_tests, parameter_space=np.linspace(0, 1, 100),
+                              figsize=(10, 10), **kwargs):
     """ Visualize the prior update.
-
-    Args:
-        - prior:
-        - likelyhood:
-
-    Returns:
-        - None
     """
-    prior = np.array(prior)
-    likelyhood = np.array(likelyhood)
-    posterior = compute_sp_posterior(
-        prior=prior,
-        likelyhood=likelyhood
-    )
+    remapper = {0: 'Healthy', 1: 'Sick'}
+    text_box = '''
+        Number of Tests: {}\n
+        Number of Positives: {}\n
+        MAP: {}\n
+        Upper: {}\n
+        Lower: {}
+    '''
 
-    data = {
-        'Prior': np.random.choice(
-            a=parameter_space,
-            size=len(parameter_space),
-            p=prior / prior.sum()
-        ),
-        'Likelyhood': np.random.choice(
-            a=parameter_space,
-            size=len(parameter_space),
-            p=likelyhood / likelyhood.sum()
-        ),
-        'Posterior': np.random.choice(
-            a=parameter_space,
-            size=len(parameter_space),
-            p=posterior
-        ),
-    }
+    total_tests_outcomes = 0
+    for tests in range(n_tests):
 
-    plt.figure(figsize=figsize)
-    for name, array in data.items():
+        prob = binom.pmf(
+            total_tests_outcomes,
+            tests,
+            p=parameter_space
+        )
+        if tests < 1:
+            prob = prob / prob.sum()
 
-        sns.distplot(
-            array,
-            label=name,
-            **kwargs
+        stats = compute_stats(
+            array=prob,
+            ci=95,
+            precis=2
         )
 
-    plt.xlabel('\u03B8')
-    plt.ylabel('Density')
-    plt.legend()
-    plt.show()
+        fig = plt.figure(figsize=figsize)
+        plt.plot(
+            parameter_space,
+            prob,
+            **kwargs
+        )
+        plt.ylim(0, 1)
+        plt.xlim(0, 1)
+        plt.ylabel('Plausibility')
+        plt.xlabel('Proportion of Sick People')
+
+        plt.text(
+            1.1,
+            0.9,
+            text_box.format(
+                tests,
+                total_tests_outcomes,
+                stats['median'],
+                stats['lower'],
+                stats['upper']
+            ),
+            verticalalignment='top'
+        )
+        if tests > 0:
+            plt.title(f'Test Result: {remapper[test_outcome]}')
+
+        test_outcome = np.random.choice([0, 1])
+        total_tests_outcomes += test_outcome
+
+        display(fig)
+        clear_output(wait=True)
+        plt.close()
+        plt.pause(0.2)
+        if tests < n_tests - 1:
+            input()
+
+    return None
 
 
 def visualize_posterior_predictions():
